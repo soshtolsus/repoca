e38f9145b68cae224ab1a11df2998873fc9b03e3
Carl Johnson IV <carljohnson@zenoss.com>
2014-11-14 17:21:54 -0600

    basic notes

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#Copyright 2014 Carl Johnson IV
+
+'''
+command line rpn calculator
+'''
+
+#TODO: allow for function definition inline (ephemeral) and in config (persistent)
+
